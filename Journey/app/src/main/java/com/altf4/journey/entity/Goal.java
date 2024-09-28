4b30adf1aa55c5ec17019fce15f8188d02243674
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2024-09-28 12:34:30 -0400

    Added and completed entity classes

---
+++ Journey/app/src/main/java/com/altf4/journey/entity/Goal.java
@@ -0,0 +1,64 @@
+package com.altf4.journey.entity;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Goal {
+    private UUID id;
+    private String title;
+    private LocalDateTime startDate;
+    private int taskCompleted;
+    private ArrayList<Task> taskList;
+
+    public Goal(String title) {
+        this.id = UUID.randomUUID();
+        this.title = title;
+        this.startDate = LocalDateTime.now();
+        this.taskCompleted = 0;
+        taskList = new ArrayList<Task>();
+    }
+
+    public Goal(String title, ArrayList<Task> taskList) {
+        this.id = UUID.randomUUID();
+        this.title = title;
+        this.startDate = LocalDateTime.now();
+        this.taskCompleted = 0;
+        this.taskList = taskList;
+    }
+
+    public UUID getGoalId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public LocalDateTime getStartDate() {
+        return this.startDate;
+    }
+
+    public Double getPercentageCompletion() {
+        return (this.taskCompleted / this.taskList.size()) * 100.0;
+    }
+
+    public int getNumOfTaskCompleted() {
+        return this.taskCompleted;
+    }
+
+    public ArrayList<Task> getTaskList() {
+        return this.taskList;
+    }
+
+    public void markTaskAsCompleted(Task taskToBeCompleted) {
+        taskToBeCompleted.checkTask();
+        taskCompleted++;
+    }
+
+    public Task removeTask(Task taskToBeRemoved) {
+        this.taskList.remove(taskToBeRemoved);
+        return taskToBeRemoved;
+    }
+
+}
