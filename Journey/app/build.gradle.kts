3835528fa7dc0077420e71c686dafd24e2542b90
Rachel-8 <89033502+Rachel-8@users.noreply.github.com>
2024-09-28 18:32:00 -0400

    Got general layout and buttons for Goals and Tasks

--- Journey/app/build.gradle.kts
+++ Journey/app/build.gradle.kts
@@ -8,13 +8,14 @@ android {
 
     defaultConfig {
         applicationId = "com.altf4.journey"
-        minSdk = 26
+        minSdk = 28 // Updated from 26 to 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
 
     buildTypes {
         release {
@@ -32,12 +33,16 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.volley)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.activity:activity:1.9.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4") // New dependency
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
