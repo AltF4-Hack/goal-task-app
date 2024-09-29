42cc2eeec260466110e9466eb1e2c5722b310779
Xuan Pham <hpham024@uottawa.ca>
2024-09-29 00:00:45 -0400

    completed form validations for create account

--- Journey/app/build.gradle.kts
+++ Journey/app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.volley)
+    implementation(libs.guava)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
